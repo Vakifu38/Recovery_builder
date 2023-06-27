5909ddab424b52179ae0540772c768ee29415447
Vakifu38 <137867698+Vakifu38@users.noreply.github.com>
2023-06-27 17:05:41 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,11 @@
-#!/bin/bash env
-
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="a23" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_samsung_a23" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot

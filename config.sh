c28fe2a2ad5163548f5de10110fede963c147a5e
Vakifu38 <137867698+Vakifu38@users.noreply.github.com>
2023-07-04 10:28:19 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="a23" # codename used in device tree
+export DEVICE="omni_a23" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_samsung_a23" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
